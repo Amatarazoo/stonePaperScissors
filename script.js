c86a1fb9a75671a0baedb0a52a9f9a150aff8fd7
Amatarazoo <64087962+Amatarazoo@users.noreply.github.com>
2023-08-27 07:33:37 +0530

    editing code by adding return

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
 // console.log("Hello World");
 
-function playRound(playSelection,computerSelection){
-    if(playSelection.toLowerCase() == computerSelection.toLowerCase()){
-        console.log('Both choices are same');
-    }else if(playSelection.toLowerCase()== "rock" && computerSelection.toLowerCase() == "scissors"){
-        console.log('You Win! '+playSelection+' beats ' +computerSelection+'.');
-    }else if(playSelection.toLowerCase()== "scissors" && computerSelection.toLowerCase() == "paper"){
-        console.log('You Win! '+playSelection+' beats ' +computerSelection+'.');
-    }else if(playSelection.toLowerCase()== "paper" && computerSelection.toLowerCase() == "stone"){
-        console.log('You Win! '+playSelection+' beats ' +computerSelection+'.');
-    }else {
-        console.log('You Lose! '+computerSelection+' beats '+playSelection+'.');
-    }
+function playRound(playSelection, computerSelection) {
+  if (playSelection.toLowerCase() == computerSelection.toLowerCase()) {
+    return "Both choices are same";
+  } else if (
+    playSelection.toLowerCase() == "rock" &&
+    computerSelection.toLowerCase() == "scissors"
+  ) {
+     return `You Win! ${playSelection} beats ${computerSelection}.`;
+  } else if (
+    playSelection.toLowerCase() == "scissors" &&
+    computerSelection.toLowerCase() == "paper"
+  ) {
+    return `You Win! ${playSelection} beats ${computerSelection} `;
+  } else if (
+    playSelection.toLowerCase() == "paper" &&
+    computerSelection.toLowerCase() == "stone"
+  ) {
+    return `You Win! ${playSelection}  beats ${computerSelection}.`;
+  } else {
+    return `You Lose! ${computerSelection}  beats  ${playSelection}.`;
+  }
 }
-const playSelection= "rock";
+const playSelection = "rock";
 const computerSelection = getComputerChoice();
-function getComputerChoice(){
-    let arr=['Rock','Scissors','Paper'];
-    choice=arr[Math.floor(Math.random()*arr.length)];
-    return choice;
+function getComputerChoice() {
+  let arr = ["Rock", "Scissors", "Paper"];
+  choice = arr[Math.floor(Math.random() * arr.length)];
+  return choice;
 }
- console.log(playRound(playSelection,computerSelection));
+console.log(playRound(playSelection,computerSelection));
