a267d8acb9c2ce13b8f741e838b857fcc33a0f2f
Amatarazoo <64087962+Amatarazoo@users.noreply.github.com>
2023-08-28 10:40:58 +0530

    first draft of rockpaperscissors

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // console.log("Hello World");
 
-function playRound(playSelection, computerSelection) {
+const playSelection = "rock";
+
+function getComputerChoice() {
+  const arr = ["Rock", "Scissors", "Paper"];
+  choice = arr[Math.floor(Math.random() * arr.length)];
+  return choice;
+}
+const computerSelection = getComputerChoice();
+function playRound(playSelection,computerSelection) {
   if (playSelection.toLowerCase() == computerSelection.toLowerCase()) {
     return "Both choices are same";
   } else if (
@@ -22,11 +30,38 @@ function playRound(playSelection, computerSelection) {
     return `You Lose! ${computerSelection}  beats  ${playSelection}.`;
   }
 }
-const playSelection = "rock";
-const computerSelection = getComputerChoice();
-function getComputerChoice() {
-  let arr = ["Rock", "Scissors", "Paper"];
-  choice = arr[Math.floor(Math.random() * arr.length)];
-  return choice;
+
+
+// defining game function.
+function game() {
+  let winCount = 0;
+  let drawCount = 0;
+  let loseCount = 0;
+  let numRounds = prompt("Enter number of rounds.");
+  for (let i = 0; i < numRounds; i++) {
+    const computerSelection = getComputerChoice();
+    console.log(playRound(playSelection, computerSelection));
+    if (playSelection.toLowerCase() == computerSelection.toLowerCase()) {
+      drawCount += 1;
+    } else if (
+      playSelection.toLowerCase() == "rock" &&
+      computerSelection.toLowerCase() == "scissors"
+    ) {
+      winCount += 1;
+    } else if (
+      playSelection.toLowerCase() == "scissors" &&
+      computerSelection.toLowerCase() == "paper"
+    ) {
+      winCount += 1;
+    } else if (
+      playSelection.toLowerCase() == "paper" &&
+      computerSelection.toLowerCase() == "stone"
+    ) {
+      winCount += 1;
+    } else {
+      loseCount += 1;
+    }
+  }
+  return `User Win:${winCount} User lose:${loseCount} draw:${drawCount}`;
 }
-console.log(playRound(playSelection,computerSelection));
+console.log(game());
